f7b1e42c69f560fde7ecaa5514cb4e3f5989ceba
tcampbell01 <teresa.j.campbell@gmail.com>
2022-08-19 15:36:18 -0400

    operator shortcuts

--- src/academy/learnprogramming/Main.java
+++ src/academy/learnprogramming/Main.java
@@ -17,5 +17,29 @@ public class Main {
 
         result = result % 3; //remainder of (4 % 3) = 1
         System.out.println(result);
+
+
+
+        //result = result + 1;
+        result++; //1 + 1
+        System.out.println(result);
+        result --; //2-1 = 1
+        System.out.println(result);
+
+        //result = result +2;
+        result += 2; //1+2
+        System.out.println(result);
+
+        //result = result * 10;
+        result *= 10; //3*10 = 30
+        System.out.println(result);
+
+        //result = result/3;
+        result /=3; //30/3=10;
+        System.out.println(result);
+
+        //result = result -2;
+        result -=2; //10-2 =8;
+        System.out.println(result);
     }
 }
